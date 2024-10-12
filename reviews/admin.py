b51e66ac182c1a9c3e205444dbdae56f5139ecbd
Karabo Phooko <Kmphooko@gmail.com>
2024-10-12 23:30:52 +0200

    Added MovieReview model and registered in admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
+# reviews/admin.py
+
 from django.contrib import admin
+from .models import MovieReview
+
+admin.site.register(MovieReview)
+
 
 # Register your models here.
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,14 @@
+# reviews/models.py
+
 from django.db import models
+
+class MovieReview(models.Model):
+    title = models.CharField(max_length=200)
+    review_text = models.TextField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
 
 # Create your models here.
